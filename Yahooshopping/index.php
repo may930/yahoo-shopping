8beec9222600ce5c4793984b4e3f0b785b3d6b99
may930 <20247030-wakasatakato@hcs.ac.jp>
2026-08-25 10:32:50 +0900

    修正

--- Yahooshopping/index.php
+++ Yahooshopping/index.php
@@ -71,13 +71,12 @@
                     <div class="sidebar-box">
                         <h3 class="sidebar-title">カテゴリから探す</h3>
                         <ul class="sidebar-menu-list">
-                            <li><a href="product-detail.php"><span>レディースファッション</span><span class="arrow">＞</span></a></li>
-                            <li><a href="#"><span>メンズファッション</span><span class="arrow">＞</span></a></li>
-                            <li><a href="#"><span>腕時計、アクセサリー</span><span class="arrow">＞</span></a></li>
-                            <li><a href="#"><span>ベビー、キッズ、マタニティ</span><span class="arrow">＞</span></a></li>
-                            <li><a href="category.html"><span>食品</span><span class="arrow">＞</span></a></li>
-                            <li><a href="#"><span>ドリンク、水、お酒</span><span class="arrow">＞</span></a></li>
-                            <li><a href="#" class="more-link">さらに表示する</a></li>
+                            <li><a href="category_ladies_Lstmain.php"><span>レディースファッション</span><span class="arrow">＞</span></a></li>
+                            <li><a href="category_mens_Lstmain.php"><span>メンズファッション</span><span class="arrow">＞</span></a></li>
+                            <li><a href="category_accessories_Lstmain.php"><span>腕時計、アクセサリー</span><span class="arrow">＞</span></a></li>
+                            <li><a href="category_kids_Lstmain.php"><span>ベビー、キッズ、マタニティ</span><span class="arrow">＞</span></a></li>
+                            <li><a href="category_food_Lstmain.php"><span>食品</span><span class="arrow">＞</span></a></li>
+                            <li><a href="category_accessories_Lstmain.php"><span>ドリンク、水、お酒</span><span class="arrow">＞</span></a></li>
                         </ul>
                     </div>
                 </aside>
